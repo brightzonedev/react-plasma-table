81ad4e439a4e5caf5cdd5f4f064c8f43864f4cf0
afnlite <afnlite@gmail.com>
2020-09-19 06:10:21 +0300

    Create standalone table

--- src/lib/Table/index.tsx
+++ src/lib/Table/index.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 
-import TableRow from "../TableRow";
-import TableHead from "../TableHead";
+// import TableRow from "../TableRow";
+// import TableHead from "../TableHead";
 import useSort from "../hooks/useSort";
 import RenderCustomComponents from "../RenderCustomComponents";
 
@@ -37,61 +37,65 @@ export const Table: React.FC<TableProps> = ({
   const { sortedData, sort, sortConfig } = useSort(data);
 
   console.log("sort sortConfig: ", sortConfig);
-  const getClassNamesFor = (name) => {
-    if (!sortConfig) {
-      return;
+  // const getClassNamesFor = (name) => {
+  //   if (!sortConfig) {
+  //     return;
+  //   }
+  //   // eslint-disable-next-line consistent-return
+  //   return sortConfig.key === name ? sortConfig.direction : undefined;
+  // };
+
+  const onSort = (dataKey, isSortable) => {
+    if (isSortable && sort) {
+      sort(dataKey);
     }
-    // eslint-disable-next-line consistent-return
-    return sortConfig.key === name ? sortConfig.direction : undefined;
   };
 
-  if (semantic) {
-    return (
-      <table className="plasma-table" cellPadding={0} cellSpacing={0}>
-        <thead className="plasma-thead">
-          {columns?.map((c) => (
-            <th className="plasma-th">
-              <button
-                className="plasma-sort-btn"
-                type="button"
-                onClick={() => sort(c?.dataKey)}
-              >
-                {c?.name}
-              </button>
-            </th>
-          ))}
-        </thead>
-        <tbody className="plasma-body">
-          {data?.map((row, index) => (
-            <tr className="plasma-tr" key={index}>
-              {columns?.map(({ id, dataKey, component }) => (
-                <Fragment key={id}>
-                  {component && (
-                    <td className="plasma-td">
-                      {RenderCustomComponents(component, row[dataKey], row)}
-                    </td>
-                  )}
-                  {!component && row[dataKey] && (
-                    <td className="plasma-td">{row[dataKey]}</td>
-                  )}
-                </Fragment>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
-
-  if (customLogic) {
-    return <>{children}</>;
-  }
-
   return (
-    <>
-      <TableHead columns={columns} sort={sort} sortConfig={sortConfig} />
-      <TableRow data={sortedData} columns={columns} />
-      {children}
-    </>
+    <table className="plasma-table" cellPadding={0} cellSpacing={0}>
+      <thead className="plasma-thead">
+        {columns?.map(({ id, name, dataKey, sortable, ...rest }) => (
+          <th className="plasma-th" {...rest}>
+            <button
+              className="plasma-sort-btn"
+              type="button"
+              onClick={() => onSort(dataKey, sortable)}
+            >
+              {name}
+            </button>
+          </th>
+        ))}
+      </thead>
+      <tbody className="plasma-body">
+        {data?.map((row, index) => (
+          <tr className="plasma-tr" key={index}>
+            {columns?.map(({ id, dataKey, component }) => (
+              <Fragment key={id}>
+                {component && (
+                  <td className="plasma-td">
+                    {RenderCustomComponents(component, row[dataKey], row)}
+                  </td>
+                )}
+                {!component && row[dataKey] && (
+                  <td className="plasma-td">{row[dataKey]}</td>
+                )}
+              </Fragment>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
+
+  // if (customLogic) {
+  //   return <>{children}</>;
+  // }
+
+  // return (
+  //   <>
+  //     <TableHead columns={columns} sort={sort} sortConfig={sortConfig} />
+  //     <TableRow data={sortedData} columns={columns} />
+  //     {children}
+  //   </>
+  // );
 };
